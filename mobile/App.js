2f6d91862044c4f39e7a9c4862e1ab8b09691f4c
Matheus Silva <matheuskazx@gmail.com>
2020-03-31 01:23:04 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+// <div> <span> <p> <h1> <header> <footer> // ReactJS
+// View = header, span. // ReactNative
+// Text = textos // ReactNative
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
